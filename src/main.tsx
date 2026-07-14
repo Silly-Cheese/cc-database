a67cf0cdaef068b34261acf3c61ffc4ef2030207
Executive_Eagle <christophershelley257@gmail.com>
2026-07-13 23:46:59 -0500

    fix(deploy): use hash routing on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import PortalRoot from './PortalRoot';
 import './styles.css';
 import './phase3.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><AuthProvider><PortalRoot/></AuthProvider></BrowserRouter></React.StrictMode>);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <AuthProvider>
+        <PortalRoot />
+      </AuthProvider>
+    </HashRouter>
+  </React.StrictMode>,
+);
