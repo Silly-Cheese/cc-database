01590861b629f6945a35cb13a553fb58c83a6774
Executive_Eagle <christophershelley257@gmail.com>
2026-07-13 23:43:58 -0500

    chore(firebase): remove unused Storage and Messaging configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,10 +6,9 @@ const firebaseConfig = {
   apiKey: 'AIzaSyDXxh48yiFHMqL4dH82-fTg0dZXqFi1ud4',
   authDomain: 'cc-database-19dba.firebaseapp.com',
   projectId: 'cc-database-19dba',
-  storageBucket: 'cc-database-19dba.firebasestorage.app',
-  messagingSenderId: '793192077235',
   appId: '1:793192077235:web:ff335960e78a39ac971dc6',
 };
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 void setPersistence(auth, browserLocalPersistence);
